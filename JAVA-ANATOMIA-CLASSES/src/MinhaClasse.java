b05d9afcb4aacf7d42df9f148a814bf1d8c015f0
PabloJ4 <008pablo008@gmail.com>
2023-03-01 19:28:31 -0300

    inicio do bootcamp

---
+++ JAVA-ANATOMIA-CLASSES/src/MinhaClasse.java
@@ -0,0 +1,17 @@
+public class MinhaClasse{
+    public static void main(String[] args) throws Exception {
+       String primeiroNome = "Pablo";
+       String segundoNome = "dos Santos";
+
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+       System.out.println(nomeCompleto);
+
+
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método" + primeiroNome.concat(" ").concat(segundoNome);
+         
+        
+    }
+}
