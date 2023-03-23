84c1454643550593710736a7f59037ff5cd13cf9
PabloJ4 <008pablo008@gmail.com>
2023-03-23 19:40:47 -0300

    Lista duplamente encadeada

---
+++ Listaduplamenteencadeada/src/NoDuplo.java
@@ -0,0 +1,58 @@
+public class NoDuplo<T> {
+
+    private T conteudo;
+    public T getConteudo() {
+        return conteudo;
+    }
+
+
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+
+
+    private NoDuplo<T> noProximo;
+    public NoDuplo<T> getNoProximo() {
+        return noProximo;
+    }
+
+
+
+    public void setNoProximo(NoDuplo<T> noProximo) {
+        this.noProximo = noProximo;
+    }
+
+
+
+    private NoDuplo<T> noPrevio;
+
+
+
+    public NoDuplo<T> getNoPrevio() {
+        return noPrevio;
+    }
+
+
+
+    public void setNoPrevio(NoDuplo<T> noPrevio) {
+        this.noPrevio = noPrevio;
+    }
+
+
+
+    public NoDuplo(T conteudo){
+        this.conteudo = conteudo;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "NoDuplo [conteudo=" + conteudo + "]";
+    }
+    
+    
+    
+}
